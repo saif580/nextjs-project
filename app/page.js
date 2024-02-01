83db74d27eadb0f0d285bff47cb9247cc5fb7899
saif580 <saifulhasan580@gmail.com>
2024-02-01 21:58:53 +0000

    intro

--- app/page.js
+++ app/page.js
@@ -1,8 +1,10 @@
 export default function Home() {
+  //Will not be seen on client side only on server side i.e terminal
+  console.log("hello world");
   return (
     <main>
       <img src="/logo.png" alt="A server surrounded by magic sparkles." />
-      <h1>Welcome to this NextJS Course!</h1>
+      <h1>Welcome to this NextJS Course !</h1>
       <p>🔥 Let&apos;s get started! 🔥</p>
     </main>
   );
