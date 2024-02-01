d51669480686d1f13f76598ef761544ad51c619a
saif580 <saifulhasan580@gmail.com>
2024-02-01 22:13:57 +0000

    Adding pages

---
+++ app/about/page.js
@@ -0,0 +1,15 @@
+// Next js relies on reserved, special file name
+// page.js-Defines pages content
+// layout.js-defines wrapper around pages
+// not-found.js-define not found callback page
+// error-define error fallback page
+
+const AboutPage = () => {
+  return (
+    <main>
+      <h1>hello, About Us </h1>
+    </main>
+  );
+};
+
+export default AboutPage;
--- app/page.js
+++ app/page.js
@@ -1,3 +1,6 @@
+import Link from "next/link";
+
+//Root page
 export default function Home() {
   //Will not be seen on client side only on server side i.e terminal
   console.log("hello world");
@@ -6,6 +9,10 @@ export default function Home() {
       <img src="/logo.png" alt="A server surrounded by magic sparkles." />
       <h1>Welcome to this NextJS Course !</h1>
       <p>🔥 Let&apos;s get started! 🔥</p>
+      <p>
+        {/* We use Link component to navigate to other pages followed by href*/}
+        <Link href="/about">About Us</Link>
+      </p>
     </main>
   );
 }
